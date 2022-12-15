d245abcbf949f5c39ccaa2d42a792d232a5df46e
Nilesh Verma <60151941+newton00009@users.noreply.github.com>
2022-12-16 00:21:42 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,36 +1,31 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-          i <- NULL
+setwd('C:/Users/rubind1/Documents/Coursera-R')
+##
+## I simply set the input x as a matrix
+## and then set the solved value "s" as a null
+## then I changed every reference to "mean" to "solve"
+makeCacheMatrix <- function(x = matrix(sample(1:100,9),3,3)) {
+  s <- NULL
   set <- function(y) {
-          x <<- y
-          i <<- NULL
+    x <<- y
+    s <<- NULL
   }
   get <- function() x
-  setinverse <- function(inverse) i <<- inverse
-  getinverse <- function() i
-  list(set = set,
-       get = get,
-       setinverse = setinverse,
-       getinverse = getinverse)
-
+  setsolve <- function(solve) s <<- solve
+  getsolve <- function() s
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
 }
-
-
-## Write a short comment describing this function
-
+##
+## Same here, changed "mean" to "solve" and "m" to "s"
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-          i <- x$getinverse()
-  if (!is.null(i)) {
-          message("getting cached data")
-          return(i)
+  s <- x$getsolve()
+  if(!is.null(s)) {
+    message("getting inversed matrix")
+    return(s)
   }
   data <- x$get()
-  i <- solve(data, ...)
-  x$setinverse(i)
-  i
+  s <- solve(data, ...)
+  x$setsolve(s)
+  s
 }
